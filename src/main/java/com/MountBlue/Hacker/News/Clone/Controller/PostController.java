65180b56a37cdb7b5cacd9781a8c099ff6c90bb7
M Gopal Krishna <mgopalkrishna2001@gmail.com>
2023-06-17 18:35:17 +0530

    Started with the home page

--- src/main/java/com/MountBlue/Hacker/News/Clone/Controller/PostController.java
+++ src/main/java/com/MountBlue/Hacker/News/Clone/Controller/PostController.java
@@ -1,4 +1,25 @@
 package com.MountBlue.Hacker.News.Clone.Controller;
 
+import com.MountBlue.Hacker.News.Clone.model.Post;
+import com.MountBlue.Hacker.News.Clone.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/HackerNews")
 public class PostController {
+    private final PostService postService;
+    public PostController(PostService postService){
+        this.postService=postService;
+    }
+    @GetMapping({"/home","/"})
+    public String getHomePage(Model model){
+        List<Post> postList = postService.getAllPost();
+        model.addAttribute("postList",postList);
+        return "home";
+    }
 }
