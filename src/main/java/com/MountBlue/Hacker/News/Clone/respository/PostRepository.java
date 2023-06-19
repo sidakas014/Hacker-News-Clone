48ff87d99c0d48ce0076e50934279ca11758814d
siddharth gupta <sidakash014@gmail.com>
2023-06-19 09:48:36 +0530

    worked on search feature and search page

--- src/main/java/com/MountBlue/Hacker/News/Clone/respository/PostRepository.java
+++ src/main/java/com/MountBlue/Hacker/News/Clone/respository/PostRepository.java
@@ -4,6 +4,9 @@ import com.MountBlue.Hacker.News.Clone.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post,Integer> {
+    List<Post> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseOrAuthorContainingIgnoreCase(String search, String search1, String search2);
 }
